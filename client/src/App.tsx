5e4ef54d2ce7f3a4ea91c9e171858618fe4241bc
AHMED-AK <scak206@gmail.com>
2026-03-01 01:10:12 +0300

    Configure GitHub Pages and automated deployment

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import { Toaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import NotFound from "@/pages/NotFound";
-import { Route, Switch } from "wouter";
+import { Route, Switch, Router as WouterRouter } from "wouter";
 import ErrorBoundary from "./components/ErrorBoundary";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import { PointsProvider } from "./contexts/PointsContext";
@@ -20,26 +20,28 @@ import Privacy from "./pages/Privacy";
 
 function Router() {
   return (
-    <div className="min-h-screen flex flex-col" dir="rtl">
-      <Navbar />
-      <main className="flex-1">
-        <Switch>
-          <Route path="/" component={Home} />
-          <Route path="/stories" component={Stories} />
-          <Route path="/stories/:id" component={StoryDetail} />
-          <Route path="/videos" component={Videos} />
-          <Route path="/games" component={Games} />
-          <Route path="/games/:id" component={GamePlay} />
-          <Route path="/puzzles" component={Puzzles} />
-          <Route path="/about" component={About} />
-          <Route path="/contact" component={Contact} />
-          <Route path="/privacy" component={Privacy} />
-          <Route path="/404" component={NotFound} />
-          <Route component={NotFound} />
-        </Switch>
-      </main>
-      <Footer />
-    </div>
+    <WouterRouter base={import.meta.env.BASE_URL.replace(/\/$/, "")}>
+      <div className="min-h-screen flex flex-col" dir="rtl">
+        <Navbar />
+        <main className="flex-1">
+          <Switch>
+            <Route path="/" component={Home} />
+            <Route path="/stories" component={Stories} />
+            <Route path="/stories/:id" component={StoryDetail} />
+            <Route path="/videos" component={Videos} />
+            <Route path="/games" component={Games} />
+            <Route path="/games/:id" component={GamePlay} />
+            <Route path="/puzzles" component={Puzzles} />
+            <Route path="/about" component={About} />
+            <Route path="/contact" component={Contact} />
+            <Route path="/privacy" component={Privacy} />
+            <Route path="/404" component={NotFound} />
+            <Route component={NotFound} />
+          </Switch>
+        </main>
+        <Footer />
+      </div>
+    </WouterRouter>
   );
 }
 
